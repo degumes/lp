de39fdba9abaddde132582ea74288cafb858902a
degumes <degumes@users.noreply.github.com>
2016-01-15 11:28:51 -0300

    olaMundo.c

---
+++ setup/olaMundo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("STanDard IO falando OI!\ncabeçalho (heads .h) padrão para Entrada e Saída\n");
+    return 0;
+}
+/*
+$ gcc -Wall -g olaMundo.c -o olaMundo.bin
+$ ./olaMundo.bin
+*/
